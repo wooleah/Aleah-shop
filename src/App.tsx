84ac99aab04bb67ec0be2b8b2700c9d077cf29f6
Woojae Jun <woojaejun@childly.com>
2020-04-09 09:41:54 +0900

    adds createUserProfileDocument

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { HomePage } from "./pages/homepage/homepage.component";
 import { ShopPage } from './pages/shop/shop.component';
 import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Header } from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 type AppProps = {}
 type AppState = {
@@ -22,10 +22,11 @@ class App extends Component<AppProps, AppState> {
   unsubscribeFromAuth: firebase.Unsubscribe | null = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
+      // this.setState({ currentUser: user });
 
+      console.log(user);
     })
   }
 
--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -21,6 +21,34 @@ provider.addScope('profile');
 provider.addScope('email');
 // Make user select an account
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export const signInWithGoogle = () => {
+  return auth.signInWithPopup(provider);
+};
+
+export const createUserProfileDocument = async (userAuth: firebase.User | null, additionalData?: {}) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error(error.toString());
+    }
+  }
+
+  return userRef;
+}
 
 export default firebase;
